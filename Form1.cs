3a963d86d6386ad579928e9b6c157f9b0f63e17d
Jan Julius <jdelang@hotmail.com>
2025-03-16 16:07:43 +0100

    add setings

--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,8 @@ namespace LostKit
 {
     public partial class Form1 : Form
     {
+        public WebView2 webView;
+
         public Form1()
         {
             InitializeComponent();
@@ -13,14 +15,30 @@ namespace LostKit
 
         private void RenderWebPage()
         {
-            var webView = new WebView2
+            webView = new WebView2
             {
                 Dock = DockStyle.Fill,
             };
             this.Controls.Add(webView);
 
-            webView.Source = new Uri("https://2004.lostcity.rs/detail");
+            webView.Source = new Uri($"https://2004.lostcity.rs/client?world={Settings.FavWorld}&detail={Settings.FavDetailSettings.ToString().ToLower()}&method=0");
             webView.EnsureCoreWebView2Async();
+
+            webView.NavigationCompleted += (s, e) => RemoveGameFrameTop();
+
+        }
+
+        private async void RemoveGameFrameTop()
+        {
+            //if (Settings.ShowTopBar)
+            //    return;
+
+            if (webView.CoreWebView2 != null)
+            {
+                await webView.CoreWebView2.ExecuteScriptAsync(
+                    "document.getElementById('gameframe-top')?.remove();"
+                );
+            }
         }
     }
 }
